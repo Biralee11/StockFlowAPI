0b577815c695b70fd51dc24744d86238992df940
Eyebira Odugba <biraodugba@gmail.com>
2026-05-21 23:43:49 +0100

    Enable Swagger in all environments for portfolio

--- StockFlowAPI/Program.cs
+++ StockFlowAPI/Program.cs
@@ -78,11 +78,9 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
